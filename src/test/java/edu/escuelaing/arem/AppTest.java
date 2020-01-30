caf2211dd195ec931b9f2b3347b9309df9f6b740
Nicolas Cardenas <nicocrash97@hotmail.com>
2020-01-30 13:36:41 -0500

    cirleci

--- src/test/java/edu/escuelaing/arem/AppTest.java
+++ src/test/java/edu/escuelaing/arem/AppTest.java
@@ -1,38 +1,89 @@
 package edu.escuelaing.arem;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+     * Rigorous Test :-)
      */
-    public AppTest( String testName )
+    @Test
+    public void shouldSolveMeanFirstColumn()
     {
-        super( testName );
+        MyMeanStandardLinkedList list = new MyMeanStandardLinkedList();
+        list.add(160);
+        list.add(591);
+        list.add(114);
+        list.add(229);
+        list.add(230);
+        list.add(270);
+        list.add(128);
+        list.add(1657);
+        list.add(624);
+        list.add(1503);
+        double mean = list.calcMean();
+
+        assertTrue( mean == 550.0 );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
+    @Test
+    public void shouldSolveStandardFirstColumn()
     {
-        return new TestSuite( AppTest.class );
+        MyMeanStandardLinkedList list = new MyMeanStandardLinkedList();
+        list.add(160);
+        list.add(591);
+        list.add(114);
+        list.add(229);
+        list.add(230);
+        list.add(270);
+        list.add(128);
+        list.add(1657);
+        list.add(624);
+        list.add(1503);
+        double standard = list.calcStandard();
+        assertTrue( standard == 411.0960958218893 );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    @Test
+    public void shouldSolveMeanSecondColumn()
     {
-        assertTrue( true );
+        MyMeanStandardLinkedList list = new MyMeanStandardLinkedList();
+        list.add(15.0);
+        list.add(69.9);
+        list.add(6.5);
+        list.add(22.4);
+        list.add(28.4);
+        list.add(65.9);
+        list.add(19.4);
+        list.add(198.7);
+        list.add(38.8);
+        list.add(138.2);
+        double mean = list.calcMean();
+        assertTrue( mean == 59.0 );
     }
+
+
+    @Test
+    public void shouldSolveStandardSecondColumn()
+    {
+        MyMeanStandardLinkedList list = new MyMeanStandardLinkedList();
+        list.add(15.0);
+        list.add(69.9);
+        list.add(6.5);
+        list.add(22.4);
+        list.add(28.4);
+        list.add(65.9);
+        list.add(19.4);
+        list.add(198.7);
+        list.add(38.8);
+        list.add(138.2);
+        double standard = list.calcStandard();
+        assertTrue( standard ==  46.380072349136235);
+    }
+
 }
